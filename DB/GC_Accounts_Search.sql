762f8bbf389caf5fe351101a4e94997e700eaefc
abdelrady <abdelrady@gmail.com>
2016-07-01 04:42:12 +0200

    adding stored procedure to repo

---
+++ DB/GC_Accounts_Search.sql
@@ -0,0 +1,65 @@
+Alter PROCEDURE SearchAccounts
+    @index int,   
+    @searchTerm nvarchar(150),
+	@resultsCount int 
+AS   
+    SET NOCOUNT ON;  
+	declare @condition nvarchar(250),
+			@query nvarchar(2048)
+	set @query = 'SELECT distinct TOP '+CAST(@resultsCount AS NVARCHAR(10))
+	select @condition = '1=1'
+
+	if(@index = 4)
+		Begin
+			set @condition = 'ACTNUMBR_4 like ''%'+ @searchTerm+'%'''
+			set @query = @query + 
+			' 0 TemplateID
+				,0 DepartmentID
+				,0 [Order]
+				,'''' Fund
+				,'''' Dept
+				,'''' Program
+				,LTRIM(RTRIM([ACTNUMBR_4])) Project
+				,'''' BaseElementObjectDetail
+				,'''' Description'
+		End
+	else if(@index = 5)
+		Begin
+			set @condition = 'ACTNUMBR_5 like '''+ @searchTerm+'%'''
+			set @query = @query + 
+			' 0 TemplateID
+				,0 DepartmentID
+				,0 [Order]
+				,'''' Fund
+				,'''' Dept
+				,'''' Program
+				,'''' Project
+				,LTRIM(RTRIM([ACTNUMBR_5])) BaseElementObjectDetail
+				,'''' Description'
+		End
+    else if(@index = 6)
+		BEGIN
+			set @condition = 'ACTDESCR like '''+ @searchTerm+'%'''
+			set @query = @query + 
+			' [ACTINDX] TemplateID
+				,0 DepartmentID
+				,0 [Order]
+				,LTRIM(RTRIM([ACTNUMBR_1])) Fund
+				,LTRIM(RTRIM([ACTNUMBR_2])) Dept
+				,LTRIM(RTRIM([ACTNUMBR_3])) Program
+				,LTRIM(RTRIM([ACTNUMBR_4])) Project
+				,LTRIM(RTRIM([ACTNUMBR_5])) BaseElementObjectDetail
+				,LTRIM(RTRIM([ACTDESCR])) Description'
+		END
+
+	set @query = @query + ' FROM [GC].[dbo].[GL00100]
+	WHERE Active = 1 and '+@condition
+	--EXECUTE(@query)
+	--print @query
+	EXECUTE sp_executesql @query
+GO 
+
+--exec SearchAccounts @index = 6, @searchTerm = N'CASH', @resultsCount = 200
+--exec SearchAccounts @index = 5, @searchTerm = N'5486', @resultsCount = 200
+--exec SearchAccounts @index = 4, @searchTerm = N'91', @resultsCount = 2000
+
