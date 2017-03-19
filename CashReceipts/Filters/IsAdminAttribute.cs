f853572dfefcb3adbb578b04420b8aa11e09abd6
abdelrady <abdelrady@gmail.com>
2017-03-19 20:00:13 +0200

    Fix isAdmin attribute issue

--- CashReceipts/Filters/IsAdminAttribute.cs
+++ CashReceipts/Filters/IsAdminAttribute.cs
@@ -12,7 +12,7 @@ namespace CashReceipts.Filters
         {
             var userName = filterContext.HttpContext.User.Identity.Name;
             var user = _db.Users.FirstOrDefault(u => u.UserName == userName);
-            if (user?.RoleId != _db.Roles.Single(x => x.Name == "Administrations").Id)
+            if (user?.RoleId != "6eae8487-db3e-46af-af20-1a63307ae86c")
                 filterContext.Result = new RedirectResult("~/Error/NotAuthorized");
             base.OnActionExecuting(filterContext);
         }
